3b1fdce24c84c2d571ac20bf39e9fba662012a2b
aahunter <ahunter@ccg.murdoch.edu.au>
2016-12-08 21:59:36 +0800

    update develop.sh in share

--- src/share/develop.sh
+++ src/share/develop.sh
@@ -30,9 +30,9 @@ if [ "$(uname)" != "Darwin" ]; then
     export DOCKER_ROUTE
 fi
 
-TTY_OPTS='--interactive --tty'
-if echo $- | grep -q "i" ; then
-    TTY_OPTS=''
+TTY_OPTS=''
+if echo xe | grep -q "i" ; then
+    TTY_OPTS='--interactive --tty'
 fi
 
 ENV_OPTS="$(env | sort | cut -d= -f1 | grep "^CCG_[a-zA-Z0-9_]*$" | awk '{print "-e", $1}')"
