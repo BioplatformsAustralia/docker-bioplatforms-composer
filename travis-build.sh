981c86efa273375ddddb7d080d7fb2e97b994794
Grahame Bowland <gbowland@ccg.murdoch.edu.au>
2018-07-24 13:29:36 +0800

    build via Travis

---
+++ travis-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e 
+
+#
+# Development build and tests
+#
+
+# ccg-composer runs as this UID, and needs to be able to
+# create output directories within it
+mkdir -p data/
+sudo chown 1000:1000 data/
+
+./develop.sh build latest
+./develop.sh skeleton
+./develop.sh build latest
